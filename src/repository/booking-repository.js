2fa961ce58a29991f78392f93ba081734930c209
Love Jindal <lkjindal123@gmail.com>
2026-07-14 01:02:40 +0530

    Added crate booking repository code

--- src/repository/booking-repository.js
+++ src/repository/booking-repository.js
@@ -5,13 +5,18 @@ const { ValidationError, AppError } = require('../utils/errors/index');
 class BookingRepository {
     async create(data){
         try {
-            
+            const booking = await Booking.create(data);
+            return booking;
         } catch (err) {
             if(err.name == 'SequelizeValidationError'){
                 throw new ValidationError(err);
             }
             throw new AppError('RepositoryError', 'Cannot create Booking', 'There was issue creating the booking, please try again later', StatusCodes.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    async update(data) {
+        
     }
 }
 
