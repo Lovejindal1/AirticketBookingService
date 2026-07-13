231d5a9955e6183054726c0549c65b45e754eaa1
Love Jindal <lkjindal123@gmail.com>
2026-07-14 00:49:08 +0530

    Added booking model

---
+++ src/repository/booking-repository.js
@@ -0,0 +1,18 @@
+const {StatusCodes} = require('http-status-codes')
+const {Booking} = require('../models/index');
+const { ValidationError, AppError } = require('../utils/errors/index');
+
+class BookingRepository {
+    async create(data){
+        try {
+            
+        } catch (err) {
+            if(err.name == 'SequelizeValidationError'){
+                throw new ValidationError(err);
+            }
+            throw new AppError('RepositoryError', 'Cannot create Booking', 'There was issue creating the booking, please try again later', StatusCodes.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
+
+module.exports = BookingRepository;
